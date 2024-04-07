4635611f520225df2ad029ba52dc94c7c7d13a1f
Shamit2105 <161947229+Shamit2105@users.noreply.github.com>
2024-04-07 18:59:06 +0530

    Update ttmyv2.cpp
    improved output formatting and ensured no faculty clashes and ensured that faculty having ! as their last character will have break

--- second_version/ttmyv2.cpp
+++ second_version/ttmyv2.cpp
@@ -27,7 +27,10 @@ public:
 
     // Days (columns) from Monday to Friday
     vector<string> days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-    // We initialize the timetable grid,three strings in tuple are course_id, classroom and faculty
+    /*
+    We initialize the timetable grid,outermost is the timeslot for each semester(timeslot*semester),middle one is the days of week and
+    third one is vector of tuple of three strings, three strings in tuple are course_id, classroom and faculty
+    */
     vector<vector<vector<tuple<string, string, string>>>> timetable;
     TTGen()
     {
@@ -50,7 +53,6 @@ public:
         credits.resize(22);
         faculty.resize(22);
         no_of_hours.resize(22);
-        int oes=odd_even_sem;//To print the semester in cmd output
         for (int i = 0; i < 22; i++)
         {
             if(i==12||i==13||i==16||i==17||i==18)
@@ -72,8 +74,6 @@ public:
             int num_of_courses;
             
             cin >> num_of_courses;
-            
-            oes+=2;
             courses[i].resize(num_of_courses);
             course_name[i].resize(num_of_courses);
             credits[i].resize(num_of_courses);
@@ -104,61 +104,68 @@ public:
     }
     void generateTimetable()
     {
-    
+        //2D vector to keep track of lecture assigned
         vector<vector<int>> lectures_assigned(courses.size(), vector<int>(courses[0].size(), 0));
         // Define a map to store the faculty assigned to each time slot and day
         map<pair<int, int>, unordered_set<string>> faculty_assigned;
+        //Map to keep track of last lectures of faculty so that if need we can provide them a break
         map<pair<int, int>, string> last_faculty;
         for (int i = 0; i < time_slots.size(); ++i)
         {
+            //At every new time slot, faculty will always be available
             bool faculty_available = true;
             // Loop through each semester (subrow)
             for (int semester = 0; semester < 22; ++semester)
             {
+                //For our program, no. of classrooms are equal to no. of semesters so each classroom is assigned to semester
                 string cr = classrooms[semester]; // Classroom for the semester
                 // Loop through each day (column)
                 for (int j = 0; j < days.size(); ++j)
                 {
-                    
                     // Assign courses to this time slot, semester, and day
-                    bool course_assigned = false;
+                    bool course_assigned = false;//Currently no courses have been assigned
                     
-                    for (int all_courses = 0; all_courses < courses.size(); ++all_courses)
+                    for (int sem_courses = 0; sem_courses < courses.size(); ++sem_courses)
                     {
-                        for (int course_index = 0; course_index < courses[all_courses].size(); ++course_index)
+                        for (int course_index = 0; course_index < courses[sem_courses].size(); ++course_index)
                         {
                             // Check if this course belongs to the current semester
-                            if (semester == all_courses)
+                            if (semester == sem_courses)
                             {
                                 // Check if there are remaining lectures for this course
-                                if (lectures_assigned[all_courses][course_index] < no_of_hours[all_courses][course_index])
+                                if (lectures_assigned[sem_courses][course_index] < no_of_hours[sem_courses][course_index])
                                 {
-                                    string course_id = courses[all_courses][course_index];
-                                    string faculty_assigned_to_course = faculty[all_courses][course_index];
-
+                                    // Get the course ID and the faculty assigned to teach this course so that we can add it to our timetable vector
+                                    string course_id = courses[sem_courses][course_index];
+                                    string faculty_assigned_to_course = faculty[sem_courses][course_index];
+                                    
                                     // Check if the faculty is already assigned to teach another course during this time slot and day
                                     bool faculty_available = true;
                                     
-                                
+                                        //Check for faculty clash
                                         if (faculty_assigned[{i, j}].count(faculty_assigned_to_course) > 0)
                                         {
                                             faculty_available = false;
                                             break;
                                         }
-                                        if (last_faculty.count({i - 1, j}) && faculty_assigned_to_course.back() == '!' && last_faculty[{i - 1, j}].back() == '!')
+                                        // Check if the faculty has taught in previous time slot and if he/she wants a break
+                                        for (int prev_slot = 0; prev_slot < i; ++prev_slot) 
                                         {
-                                            faculty_available = false;
-                                            break;
-                                        }
-                                    
-
-                                    if (faculty_available)
+                                            if (last_faculty.count({prev_slot, j}) && last_faculty[{prev_slot, j}].back() == '!') 
+                                            {
+                                                // If a break is found in any previous time slot on the same day, faculty is not available
+                                                faculty_available = false;
+                                                break; // No need to check further
+                                            }
+                                        }                                    
+                                    if (faculty_available)//Assigning courses only if there are no faculty clashes and the faculty does not need a break
                                     {
                                         // Assign the course to this time slot, semester, and day
                                         timetable[22 * i + semester][j].push_back({course_id, cr, faculty_assigned_to_course});
-                                        lectures_assigned[all_courses][course_index]++;
+                                        lectures_assigned[sem_courses][course_index]++;
                                         faculty_assigned[{i, j}].insert(faculty_assigned_to_course);
                                         course_assigned = true;
+                                        last_faculty[{i,j}]=(faculty_assigned_to_course);
                                         break; // Move to the next semester
                                     }
                                 }
@@ -229,50 +236,83 @@ public:
                         outfile <<"\n(Semester 1)\n";
                     }
                     
-                    
-                    if(i%22<12&&(i+1)%3==1)
-                    {
-                        outfile << "ICT   \t\t";
-                    }
-                    else if(i%22<12&&(i+1)%3==2)
-                    {
-                        outfile << "ICT+CS\t\t";
-                    }
-                    else if(i%22<12&&(i+1)%3==0)
-                    {
-                        outfile << "MnC\t\t";
-                    }
-                    else if(i%22<16&&i%22>=12&&(i+1)%2==1)
-                    {
-                        outfile << "ICT\t\t";
-                    }
-                    else if(i%22<16&&i%22>=12&&(i+1)%2==0)
-                        outfile << "EC\t\t";
-                    else if(i%22>=16&&(i+1)%3==2)
-                    {
-                        outfile << "IT\t\t";
-                    }
-                    else if(i%22>=16&&(i+1)%3==0)
-                    {
-                        outfile << "DS\t\t";
-                    }
-                    else if(i%22>=16&&(i+1)%3==1)
-                    {
-                        outfile << "AA\t\t";
-                    }
-                    else
-                        outfile << "\n\t\t";
-                    
-                    
-                    
                 }
                 else
                 {
-                    if(((i%12))%2==0)
-                        outfile<<"\n(Semester " << to_string((i/12)+2) << ")\t"; // Time slot with even semester info
-                    else
-                        outfile <<"\n\t\t";
-                }   
+                    int x=i;
+                    if(i%22==0)
+                    {
+                        outfile << "\nBTech:"<<endl;
+                    }
+                    else if(i%22==12)
+                    {
+                        outfile<<"\nMTech:"<<endl;
+                    }
+                    else if (i%22==16)
+                        outfile<<"\nMSc:"<<endl;
+                    if(((i%22))%3==0&&((i%22)+1!=22)&&((i%22)+1!=13)&&i%22<12)
+                    {  
+                        
+                        int semester_number = (i / 22 * 4 + (i % 22) / 3) % 4*2 + 2; // Calculating even semester number
+                        outfile << "\n(Semester " << to_string(semester_number) << ")\n"; // even semester info
+                    }
+                    else if((i%22)+1==15||(i%22)+1==20)
+                    {
+                        outfile <<"\n(Semester 4)\n";
+                    }
+                    else if((i%22)+1==13||(i%22)+1==17)
+                    {
+                        outfile <<"\n(Semester 2)\n";
+                    }
+                }    
+                    if(i%22<6&&(i%22==0||(i%22)%3==0))
+                    {
+                        outfile << "ICT   \t\t";
+                    }
+                    else if(i%22<6&&(i%22==0||(i%22)%3==1))
+                    {
+                        outfile << "ICT+CS\t\t";
+                    }
+                    else if(i%22<6&&(i%22==0||(i%22)%3==2))
+                    {
+                        outfile << "MnC   \t\t";
+                    }
+                    if(i%22>=6&&i%22<12&&(i%22==0||(i%22)%3==0))
+                    {
+                        outfile << "ICT+CS\t\t";
+                    }
+                    else if(i%22>=6&&i%22<12&&(i%22==0||(i%22)%3==1))
+                    {
+                        outfile << "ICT+CS\t\t";
+                    }
+                    else if(i%22>=6&&i%22<12&&(i%22==0||(i%22)%3==2))
+                    {
+                        outfile << "MnC   \t\t";
+                    }
+                    else if((i%22<16&&i%22>=12)&&((i%22)%2==0))
+                    {
+                        outfile << "ICT   \t\t";
+                    }
+                    else if((i%22<16&&i%22>=12)&&((i%22)%2==1))
+                        outfile << "EC    \t\t";
+                    else if((i%22>=16)&&((i%22)%3==1))
+                    {
+                        outfile << "IT    \t\t";
+                    }
+                    else if((i%22>=16)&&((i%22)%3==2))
+                    {
+                        outfile << "DS    \t\t";
+                    }
+                    else if((i%22>=16)&&((i%22)%3==0))
+                    {
+                        outfile << "AA    \t\t";
+                    }
+                    
+                    
+                    
+                    
+                
+                
                 for (int j = 0; j < timetable[i].size(); ++j) 
                 {
                     
@@ -297,7 +337,7 @@ public:
                                     outfile <<setw(7)<<"  " <<get<0>(course_classroom_faculty) << "[" << get<1>(course_classroom_faculty) << "](" << get<2>(course_classroom_faculty) << ") \t"; // Course with classroom and faculty
                                 else
                                 {
-                                    outfile<<setw(6) <<"" <<get<0>(course_classroom_faculty) << "[" << get<1>(course_classroom_faculty) << "](" << get<2>(course_classroom_faculty) << ")\t";
+                                    outfile<<setw(7) <<"" <<get<0>(course_classroom_faculty) << "[" << get<1>(course_classroom_faculty) << "](" << get<2>(course_classroom_faculty) << ")\t";
                                 }
                                 }
                             }
@@ -356,6 +396,7 @@ public:
         }
             cout << "Faculty Timetable has been written to fac_tt.txt" << endl;
     }
+
     void displayClassroomTimetable(const string& classroom_name) 
     {
         ofstream outfile("classroom_tt.txt", ios::app); // Opening file in append mode
